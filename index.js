bf39b81cdf677356aa6b5d064621c5f88a3e75b3
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2021-02-28 11:53:20 -0300

    Refactor spare scenario

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function frameScore(frame, nextFrame) {
-  const current = frame[0] + frame[1];
-  if (current === 10) return current + nextFrame[0];
+  const current = rawPoints(frame);
+  if (isSpare(frame)) {
+    return current + nextFrame[0];
+  }
   return current;
 }
+
+const isSpare = (frame) => rawPoints(frame) === 10;
+const rawPoints = (frame) => frame[0] + frame[1];
 
 module.exports = { frameScore };
