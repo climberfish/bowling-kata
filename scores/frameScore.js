ae2db702f2c1433e02414ba340f7110f99a8b1de
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2021-02-28 14:04:50 -0300

    Remove unused functions from frameScore.js

--- scores/frameScore.js
+++ scores/frameScore.js
@@ -45,26 +45,4 @@ class Frame {
   }
 }
 
-function frameScore(frame, nextFrame, nextNextFrame) {
-  const current = rawPoints(frame);
-  if (isStrike(frame)) {
-    return strikeScore(nextFrame, nextNextFrame);
-  }
-  if (isSpare(frame)) {
-    return current + nextFrame[0];
-  }
-  return current;
-}
-
-function strikeScore(nextFrame, nextNextFrame) {
-  if (isStrike(nextFrame)) {
-    return 10 + 10 + nextNextFrame[0];
-  }
-  return 10 + rawPoints(nextFrame);
-}
-
-const isStrike = (frame) => frame[0] === 10;
-const isSpare = (frame) => rawPoints(frame) === 10;
-const rawPoints = (frame) => frame[0] + frame[1];
-
-module.exports = { Frame, frameScore, isSpare, isStrike };
+module.exports = Frame;
--- scores/index.js
+++ scores/index.js
@@ -1,4 +1,4 @@
-const { Frame } = require("./frameScore");
+const Frame = require("./frameScore");
 const Game = require("./gameScore");
 
 module.exports = { Frame, Game };
