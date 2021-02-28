9c46cb20d2b5117ac077941a415ef57dcec58689
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2021-02-28 13:46:58 -0300

    Refactor: create Game entity

--- scores/gameScore.js
+++ scores/gameScore.js
@@ -1,9 +1,24 @@
-const { frameScore, isSpare, isStrike } = require("./frameScore");
+const { Frame, frameScore, isSpare, isStrike } = require("./frameScore");
+
+class Game {
+  constructor(frames) {
+    this.frames = frames;
+  }
+
+  static fromArrays(arrays) {
+    const frames = [];
+    for (let i = arrays.length - 1; i >= 0; i--) {
+      frames.unshift(Frame.fromArray(arrays[i], frames[0]));
+    }
+    return new Game(frames);
+  }
+}
 
 function gameScore(frames) {
   let scores = 0;
+  const game = Game.fromArrays(frames);
   for (const i of Array(8).keys()) {
-    scores += frameScore(frames[i], frames[i + 1], frames[i + 2]);
+    scores += game.frames[i].score();
   }
   scores += penultFrameScore(frames[8], frames[9]);
   scores += lastFrameScore(frames[9]);
