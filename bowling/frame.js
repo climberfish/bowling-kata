cabd26a32c726c761c0c0ec9f98711c950449b9a
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2021-02-28 14:19:12 -0300

    Move all frame score logics to Frame

--- bowling/frame.js
+++ bowling/frame.js
@@ -1,20 +1,31 @@
 class Frame {
-  constructor(nextFrame, firstBall, secondBall, thirdBall) {
+  constructor(
+    nextFrame,
+    firstBall,
+    secondBall,
+    thirdBall,
+    { isLastFrame, isPenultFrame } = {}
+  ) {
     this.next = nextFrame;
     this.firstBall = firstBall;
     this.secondBall = secondBall;
     this.thirdBall = thirdBall;
+    this.isLastFrame = isLastFrame;
+    this.isPenultFrame = isPenultFrame;
   }
 
   toArray() {
     return [this.firstBall, this.secondBall, this.thirdBall];
   }
 
-  static fromArray(frame, nextFrame) {
-    return new Frame(nextFrame, frame[0], frame[1], frame[2]);
+  static fromArray(frame, nextFrame, props) {
+    return new Frame(nextFrame, frame[0], frame[1], frame[2], props);
   }
 
   score() {
+    if (this.isLastFrame) return this.__lastFrameScore();
+    if (this.isPenultFrame) return this.__penultFrameScore();
+
     const current = this.__rawPoints();
     if (this.isStrike()) {
       return this.__strikeScore();
@@ -42,6 +53,30 @@ class Frame {
       return 10 + 10 + this.next.next.firstBall;
     }
     return 10 + this.next.__rawPoints();
+  }
+
+  __lastFrameScore() {
+    if (this.isStrike()) return 10 + this.secondBall + this.thirdBall;
+    if (this.isSpare()) return 10 + this.thirdBall;
+
+    return this.__asTrivial().score();
+  }
+
+  __penultFrameScore() {
+    if (this.isStrike() && this.next.isStrike()) {
+      return 10 + 10 + this.next.firstBall;
+    }
+    return this.__asTrivial().score();
+  }
+
+  __asTrivial() {
+    return new Frame(
+      this.next,
+      this.firstBall,
+      this.secondBall,
+      this.thirdBall,
+      {}
+    );
   }
 }
 
--- bowling/game.js
+++ bowling/game.js
@@ -8,38 +8,21 @@ class Game {
   static fromArrays(arrays) {
     const frames = [];
     for (let i = arrays.length - 1; i >= 0; i--) {
-      frames.unshift(Frame.fromArray(arrays[i], frames[0]));
+      const isLastFrame = i === 9;
+      const isPenultFrame = i === 8;
+      frames.unshift(
+        Frame.fromArray(arrays[i], frames[0], { isLastFrame, isPenultFrame })
+      );
     }
     return new Game(frames);
   }
 
   score() {
     let scores = 0;
-    for (const i of Array(8).keys()) {
+    for (const i of Array(10).keys()) {
       scores += this.frames[i].score();
     }
-    scores += this.__penultFrameScore();
-    scores += this.__lastFrameScore();
     return scores;
-  }
-
-  __penultFrameScore() {
-    const [penultFrame, lastFrame] = this.frames.slice(8);
-
-    if (penultFrame.isStrike() && lastFrame.isStrike()) {
-      return 10 + 10 + lastFrame.firstBall;
-    }
-    return penultFrame.score();
-  }
-
-  __lastFrameScore() {
-    const lastFrame = this.frames[9];
-
-    if (lastFrame.isStrike())
-      return 10 + lastFrame.secondBall + lastFrame.thirdBall;
-    if (lastFrame.isSpare()) return 10 + lastFrame.thirdBall;
-
-    return lastFrame.score();
   }
 }
 
